43399d9b197a52e2f3dd9442009a79c0560e2332
silkesssji <kastrdast@mail.ru>
2022-05-19 18:50:39 +0400

    Init

---
+++ src/components/app/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from "./App.module.scss";
+
+export class App extends React.Component {
+    render() {
+        return(
+            <div className={styles.main}>
+                
+            </div>
+        )
+    }
+}
