07b0233d5fc14ef42448db27feb1a76d89310c6d
silkesssji <kastrdast@mail.ru>
2022-05-25 01:16:22 +0400

    Fix paginator

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -3,7 +3,23 @@ import React from 'react';
 import main from "./modules/main.module.scss";
 import cn from 'classnames';
 import header from "./modules/header.module.scss";
-import paginator from "./modules/paginator.module.scss"
+import paginator from "./modules/paginator.module.scss";
+import footer from "./modules/footer.module.scss";
+import logo from './index.svg';
+
+
+function debounce(fn, delay) {
+    let timer = null;
+    return function() {
+      var context = this,
+        args = arguments;
+      clearTimeout(timer);
+      timer = setTimeout(function() {
+        fn.apply(context, args);
+      }, delay);
+    };
+  }
+
 
 export class App extends React.Component {
     constructor(props) {
@@ -53,14 +69,31 @@ export class App extends React.Component {
                 }
             })
         } else {
-            this.setState({page: Number(e.target.value)})
+            this.setState({page: Number(e.target.value)});
+            if (Number(e.target.value) === this.state.totalPages) {
+                    this.setState({paginator: [
+                        this.state.totalPages - 5,
+                        this.state.totalPages - 4,
+                        this.state.totalPages - 3,
+                        this.state.totalPages - 2,
+                        this.state.totalPages - 1,
+                    ]})
+            }
+            if (Number(e.target.value) === 1) {
+                this.setState({paginator: [1, 2, 3, 4, 5]})
+            }
         }
     }
-    changeRequest = (e) => {
-        this.setState({page: 1})
-        this.setState({requestValue: e.target.value});
-        this.setState({paginator: [1, 2, 3, 4, 5]})
-    }
+    changeRequest = debounce((e) => {
+        e.preventDefault();
+        if (!(e.target.value.replace(/\s/g, '') === '')) {
+            this.setState({page: 1})
+            this.setState({requestValue: e.target.value});
+            this.setState({paginator: [1, 2, 3, 4, 5]})
+        } else {
+            this.setState({requestValue: ''});
+        }
+    }, 300)
     componentDidUpdate(prevProps, prevState) {
         const { page, requestValue } = this.state;
         if (page !== prevState.page|| requestValue !== prevState.requestValue) {
@@ -91,17 +124,19 @@ export class App extends React.Component {
                             }
                         }>
                     <h1 className={header.heading}>MOVIESEARCH</h1>
-                    <input className={header.input} type="text" placeholder='Search' onInput={this.changeRequest}></input>
+                    <input className={header.input} type="search" placeholder='Search' autoComplete="on" onChange={this.changeRequest}></input>
                 </header>
                 <main className={main.main}>
                     <div className={paginator.paginator}>
                         {<button type='button' disabled={this.state.paginator[0]===1} className={cn(paginator.button, this.state.paginator[0]===1 ? paginator.disabled : '')} value={"back"} onClick={this.changePage}>{'<'}</button>}
-                        {this.state.paginator[0] < this.state.totalPages && !(this.state.totalPages === 0) && <button type='button' className={cn(paginator.button, this.state.page === this.state.paginator[0]?paginator.current:'')} onClick={this.changePage} value={this.state.paginator[0]}>{this.state.paginator[0]}</button>}
-                        {this.state.paginator[1] < this.state.totalPages && <button type='button' className={cn(paginator.button, this.state.page === this.state.paginator[1]?paginator.current:'')} onClick={this.changePage} value={this.state.paginator[1]}>{this.state.paginator[1]}</button>}
-                        {this.state.paginator[2] < this.state.totalPages && <button type='button' className={cn(paginator.button, this.state.page === this.state.paginator[2]?paginator.current:'')} onClick={this.changePage} value={this.state.paginator[2]}>{this.state.paginator[2]}</button>}
-                        {this.state.paginator[3] < this.state.totalPages && <button type='button' className={cn(paginator.button, this.state.page === this.state.paginator[3]?paginator.current:'')} onClick={this.changePage} value={this.state.paginator[3]}>{this.state.paginator[3]}</button>}
-                        {this.state.paginator[4] < this.state.totalPages && <button type='button' className={cn(paginator.button, this.state.page === this.state.paginator[4]?paginator.current:'')} onClick={this.changePage} value={this.state.paginator[4]}>{this.state.paginator[4]}</button>}
-                        {!(this.state.paginator.includes(this.state.totalPages - 1) || this.state.paginator.includes(this.state.totalPages) || (this.state.totalPages === 0)) && <div className={paginator.dots}>...</div>}
+                        {!(this.state.paginator.includes(1)) && <button type='button' className={paginator.button} onClick={this.changePage} value={1}>1</button>}
+                        {(this.state.paginator[0] - 1 >= 4) && <div className={paginator.dots}>...</div>}
+                        {(this.state.paginator[0] < this.state.totalPages) && (this.state.paginator[0] >= 1)  && !(this.state.totalPages === 0) && <button type='button' className={cn(paginator.button, this.state.page === this.state.paginator[0]?paginator.current:'')} onClick={this.changePage} value={this.state.paginator[0]}>{this.state.paginator[0]}</button>}
+                        {(this.state.paginator[1] < this.state.totalPages) && (this.state.paginator[1] > 1) && <button type='button' className={cn(paginator.button, this.state.page === this.state.paginator[1]?paginator.current:'')} onClick={this.changePage} value={this.state.paginator[1]}>{this.state.paginator[1]}</button>}
+                        {(this.state.paginator[2] < this.state.totalPages) && (this.state.paginator[2] > 1) && <button type='button' className={cn(paginator.button, this.state.page === this.state.paginator[2]?paginator.current:'')} onClick={this.changePage} value={this.state.paginator[2]}>{this.state.paginator[2]}</button>}
+                        {(this.state.paginator[3] < this.state.totalPages) && (this.state.paginator[3] > 1) && <button type='button' className={cn(paginator.button, this.state.page === this.state.paginator[3]?paginator.current:'')} onClick={this.changePage} value={this.state.paginator[3]}>{this.state.paginator[3]}</button>}
+                        {(this.state.paginator[4] < this.state.totalPages) && (this.state.paginator[4] > 1) && <button type='button' className={cn(paginator.button, this.state.page === this.state.paginator[4]?paginator.current:'')} onClick={this.changePage} value={this.state.paginator[4]}>{this.state.paginator[4]}</button>}
+                        {(!(this.state.paginator.includes(this.state.totalPages - 1)) || this.state.paginator.includes(this.state.totalPages) || (this.state.totalPages === 0)) && ((this.state.totalPages - this.state.paginator[this.state.paginator.length - 1]) > 4) && <div className={paginator.dots}>...</div>}
                         {!(this.state.totalPages === 0) && <button type='button' disabled={this.state.page === this.state.totalPages} className={cn(paginator.button, this.state.page === this.state.totalPages?paginator.current:'')} onClick={this.changePage} value={this.state.totalPages}>{this.state.totalPages}</button>}
                         {<button type='button' disabled={!(this.state.totalPages > this.state.paginator[this.state.paginator.length - 1])} className={cn(paginator.button, !(this.state.totalPages > this.state.paginator[this.state.paginator.length - 1]) ? paginator.disabled : '')} value={"forward"} onClick={this.changePage}>{'>'}</button>}
                     </div>
@@ -137,16 +172,26 @@ export class App extends React.Component {
                     </div>
                     <div className={paginator.paginator}>
                         {<button type='button' disabled={this.state.paginator[0]===1} className={cn(paginator.button, this.state.paginator[0]===1 ? paginator.disabled : '')} value={"back"} onClick={this.changePage}>{'<'}</button>}
-                        {this.state.paginator[0] < this.state.totalPages && !(this.state.totalPages === 0) && <button type='button' className={cn(paginator.button, this.state.page === this.state.paginator[0]?paginator.current:'')} onClick={this.changePage} value={this.state.paginator[0]}>{this.state.paginator[0]}</button>}
-                        {this.state.paginator[1] < this.state.totalPages && <button type='button' className={cn(paginator.button, this.state.page === this.state.paginator[1]?paginator.current:'')} onClick={this.changePage} value={this.state.paginator[1]}>{this.state.paginator[1]}</button>}
-                        {this.state.paginator[2] < this.state.totalPages && <button type='button' className={cn(paginator.button, this.state.page === this.state.paginator[2]?paginator.current:'')} onClick={this.changePage} value={this.state.paginator[2]}>{this.state.paginator[2]}</button>}
-                        {this.state.paginator[3] < this.state.totalPages && <button type='button' className={cn(paginator.button, this.state.page === this.state.paginator[3]?paginator.current:'')} onClick={this.changePage} value={this.state.paginator[3]}>{this.state.paginator[3]}</button>}
-                        {this.state.paginator[4] < this.state.totalPages && <button type='button' className={cn(paginator.button, this.state.page === this.state.paginator[4]?paginator.current:'')} onClick={this.changePage} value={this.state.paginator[4]}>{this.state.paginator[4]}</button>}
-                        {!(this.state.paginator.includes(this.state.totalPages - 1) || this.state.paginator.includes(this.state.totalPages) || (this.state.totalPages === 0)) && <div className={paginator.dots}>...</div>}
+                        {!(this.state.paginator.includes(1)) && <button type='button' className={paginator.button} onClick={this.changePage} value={1}>1</button>}
+                        {(this.state.paginator[0] - 1 >= 4) && <div className={paginator.dots}>...</div>}
+                        {(this.state.paginator[0] < this.state.totalPages) && (this.state.paginator[0] >= 1)  && !(this.state.totalPages === 0) && <button type='button' className={cn(paginator.button, this.state.page === this.state.paginator[0]?paginator.current:'')} onClick={this.changePage} value={this.state.paginator[0]}>{this.state.paginator[0]}</button>}
+                        {(this.state.paginator[1] < this.state.totalPages) && (this.state.paginator[1] > 1) && <button type='button' className={cn(paginator.button, this.state.page === this.state.paginator[1]?paginator.current:'')} onClick={this.changePage} value={this.state.paginator[1]}>{this.state.paginator[1]}</button>}
+                        {(this.state.paginator[2] < this.state.totalPages) && (this.state.paginator[2] > 1) && <button type='button' className={cn(paginator.button, this.state.page === this.state.paginator[2]?paginator.current:'')} onClick={this.changePage} value={this.state.paginator[2]}>{this.state.paginator[2]}</button>}
+                        {(this.state.paginator[3] < this.state.totalPages) && (this.state.paginator[3] > 1) && <button type='button' className={cn(paginator.button, this.state.page === this.state.paginator[3]?paginator.current:'')} onClick={this.changePage} value={this.state.paginator[3]}>{this.state.paginator[3]}</button>}
+                        {(this.state.paginator[4] < this.state.totalPages) && (this.state.paginator[4] > 1) && <button type='button' className={cn(paginator.button, this.state.page === this.state.paginator[4]?paginator.current:'')} onClick={this.changePage} value={this.state.paginator[4]}>{this.state.paginator[4]}</button>}
+                        {(!(this.state.paginator.includes(this.state.totalPages - 1)) || this.state.paginator.includes(this.state.totalPages) || (this.state.totalPages === 0)) && ((this.state.totalPages - this.state.paginator[this.state.paginator.length - 1]) > 4) && <div className={paginator.dots}>...</div>}
                         {!(this.state.totalPages === 0) && <button type='button' disabled={this.state.page === this.state.totalPages} className={cn(paginator.button, this.state.page === this.state.totalPages?paginator.current:'')} onClick={this.changePage} value={this.state.totalPages}>{this.state.totalPages}</button>}
                         {<button type='button' disabled={!(this.state.totalPages > this.state.paginator[this.state.paginator.length - 1])} className={cn(paginator.button, !(this.state.totalPages > this.state.paginator[this.state.paginator.length - 1]) ? paginator.disabled : '')} value={"forward"} onClick={this.changePage}>{'>'}</button>}
                     </div>
                 </main>
+                <footer className={footer.footer}>
+                    <div className={footer.wrapper}>
+                        <a className={footer.link} href='https://www.themoviedb.org/'><img src={logo} alt="logo" width='200px'/></a>
+                        <a className={footer.link} href='https://github.com/silkesssji'>Silkessji GitHub</a>
+                        <a className={footer.link} href='https://github.com/silkesssji/movie-search-engine'>Repository</a>
+                        <a className={footer.link} href='https://www.themoviedb.org/'>MovieDB</a>
+                    </div>
+                </footer>
             </>
         )
     }
